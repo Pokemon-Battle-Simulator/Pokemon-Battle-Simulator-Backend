a56efbed514aa294cd14e8e56489e37a029c0ed3
Simon-Coble <simon291@revature.net>
2022-03-24 15:54:03 -0400

    project

---
+++ src/main/java/com/revature/PokemonBattleSimulatorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonBattleSimulatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonBattleSimulatorBackendApplication.class, args);
+	}
+
+}
