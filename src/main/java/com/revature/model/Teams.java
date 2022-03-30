28d577884e4636f347c9bde7388f597872e79e85
Micheal Bailey <mbaileyfuturist@gmail.com>
2022-03-30 00:01:28 -0400

    Readded the Pokemon, Teams and Session Modal classes.

---
+++ src/main/java/com/revature/model/Teams.java
@@ -0,0 +1,62 @@
+
+  
+package com.revature.model;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Component
+@Entity
+@Table(name="teams")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIdentityInfo(generator=ObjectIdGenerators.PropertyGenerator.class, property="id") 
+public class Teams {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	@Column
+	private String name;
+	
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name="user_id")
+	private User user;	
+	
+	@OneToMany(mappedBy="team", cascade = CascadeType.ALL)
+	private List<Pokemon> pokemon;
+
+	public Teams(String name, User user, List<Pokemon> pokemon) {
+		super();
+		this.name = name;
+		this.user = user;
+		this.pokemon = pokemon;
+	}
+	
+	
+}
