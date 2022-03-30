514b269a9b41b1b3daaefc4b88214aec08d36c41
unknown <jaime317@revature.net>
2022-03-30 16:29:27 -0700

    Added Swagger code

---
+++ src/main/java/com/revature/web/RootController.java
@@ -0,0 +1,18 @@
+package com.revature.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class RootController {
+	
+	@GetMapping("/docs") 
+	@ResponseStatus(HttpStatus.SEE_OTHER)
+	public RedirectView redirectViewToApiDocumentation() {
+		return new RedirectView("swagger-ui/index.html"); 
+		}
+
+}
