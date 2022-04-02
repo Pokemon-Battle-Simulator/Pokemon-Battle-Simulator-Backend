8e6fd9e87443ab5a2a01ab100b136975a04355fc
Micheal Bailey <mbaileyfuturist@gmail.com>
2022-04-02 15:49:38 -0400

    Created an exception package that contains an AuthenticationException class. Created a util package that contains a JwTTokenManager class responsible for issuing tokens for authentication. Implemented the loginUser() method in the UserController, the authenticate() method in the UserService and the findByUsername() method in the UserRepository.

--- src/main/java/com/revature/repository/UserRepository.java
+++ src/main/java/com/revature/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package com.revature.repository;
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.revature.model.User;
@@ -9,5 +12,6 @@ import com.revature.model.User;
 @Repository 
 public interface UserRepository extends JpaRepository<User, Integer> { 
 
-	
+	User findByUsername(String username);
+		
 }
